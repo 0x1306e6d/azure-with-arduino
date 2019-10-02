c2ce158887d0447af8b7f0705270111c37f4515c
Gihwan Kim <ghkim3221@gmail.com>
2019-10-02 14:53:34 +0900

    Connect to the IoT Hub and send message (#5)
    * Initialize the Serial
    
    * Initialize the WiFi and fetch time from NTP server
    
    * Create the IoT Hub client from connection string
    
    * Send json message to the IoT Hub
    
    * Turn the LED on/off when sending message

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,17 +1,150 @@
 #include "config.h"
 
+#include <time.h>
+
+#include <ArduinoJson.h>
+#include <AzureIoTHub.h>
+#include <AzureIoTProtocol_MQTT.h>
+#include <ESP8266WiFi.h>
+
+void initSerial()
+{
+  // Start serial and initialize stdout
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println("Serial.begin OK");
+}
+
+void initWifi()
+{
+  // Attempt to connect to Wifi network:
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(IOT_CONFIG_WIFI_SSID);
+
+  // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+  WiFi.begin(IOT_CONFIG_WIFI_SSID, IOT_CONFIG_WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi.begin OK");
+}
+
+void initTime()
+{
+  time_t epochTime;
+
+  configTime(0, 0, NTP_SERVER);
+
+  while (true)
+  {
+    epochTime = time(NULL);
+
+    if (epochTime < NTP_MIN_EPOCH)
+    {
+      Serial.println("configTime FAIL");
+      delay(2000);
+    }
+    else
+    {
+      Serial.print("configTime OK, epochTime: ");
+      Serial.println(epochTime);
+      break;
+    }
+  }
+}
+
+static IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle = NULL;
+static unsigned int iotHubMessageTrackingId = 0;
+
+void initIoTHubClient()
+{
+  iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(IOTHUB_CONNECTION_STRING, MQTT_Protocol);
+  if (iotHubClientHandle != NULL)
+  {
+    Serial.println("IoTHubClient_LL_CreateFromConnectionString OK");
+  }
+  else
+  {
+    Serial.println("IoTHubClient_LL_CreateFromConnectionString FAIL");
+  }
+}
+
+void sendIoTHubMessageCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void *userContextCallback)
+{
+  unsigned int messageTrackingId = (unsigned int)(uintptr_t)userContextCallback;
+
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED off
+
+  Serial.print("sendIoTHubMessageCallback ");
+  Serial.print(ENUM_TO_STRING(IOTHUB_CLIENT_CONFIRMATION_RESULT, result));
+  Serial.print(", messageTrackingId: ");
+  Serial.println(messageTrackingId);
+}
+
+void sendIoTHubMessage(const char *source)
+{
+  IOTHUB_MESSAGE_HANDLE messageHandle = IoTHubMessage_CreateFromString(source);
+  if (messageHandle == NULL)
+  {
+    Serial.println("IoTHubMessage_CreateFromString FAIL");
+  }
+  else
+  {
+    if (IoTHubClient_LL_SendEventAsync(iotHubClientHandle, messageHandle, sendIoTHubMessageCallback, (void *)(uintptr_t)iotHubMessageTrackingId) == IOTHUB_CLIENT_OK)
+    {
+      digitalWrite(LED_BUILTIN, LOW); // turn the LED on
+
+      Serial.println("IoTHubClient_LL_SendEventAsync OK");
+    }
+    else
+    {
+      Serial.println("IoTHubClient_LL_SendEventAsync FAIL");
+    }
+
+    IoTHubMessage_Destroy(messageHandle);
+  }
+
+  iotHubMessageTrackingId++;
+}
+
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+  initSerial();
+
+  initWifi();
+  initTime();
+
+  initIoTHubClient();
+
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
+static int loopCount = 0;
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  if (iotHubClientHandle != NULL)
+  {
+    if ((loopCount % 1000) == 0)
+    {
+      StaticJsonDocument<256> json;
+      json["temperature"] = 0;
+      json["humidity"] = 0;
+
+      char buffer[256];
+      serializeJson(json, buffer, 256);
+      sendIoTHubMessage(buffer);
+    }
+
+    IoTHubClient_LL_DoWork(iotHubClientHandle);
+    loopCount++;
+  }
+
+  delay(100);
 }
--- arduino/config.h
+++ arduino/config.h
@@ -3,4 +3,12 @@
 
 #define LED_BUILTIN 16
 
+#define IOTHUB_CONNECTION_STRING "HostName=<Your IoT Hub Name>.azure-devices.net;DeviceId=<Your Device Id>;SharedAccessKey=<Your Shared Access Key>"
+
+#define IOT_CONFIG_WIFI_SSID "Your WiFi SSID"
+#define IOT_CONFIG_WIFI_PASSWORD "Your WiFi Password"
+
+#define NTP_SERVER "pool.ntp.org"
+#define NTP_MIN_EPOCH (40 * 365 * 24 * 3600)
+
 #endif
