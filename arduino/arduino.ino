cd9bf96cc4bf0ff9baaea78ac1bf81f653adfa1a
Gihwan Kim <ghkim3221@gmail.com>
2019-10-02 13:32:26 +0900

    Initialize the Serial

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,8 +1,18 @@
 #include "config.h"
 
+void initSerial()
+{
+  // Start serial and initialize stdout
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println("Serial.begin OK");
+}
+
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+  initSerial();
+
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
